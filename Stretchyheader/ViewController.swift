9a80eed440dfd73ca40379858cc3d21933aced3f
sdik <ahmadshiddiq0@gmail.com>
2019-02-21 13:37:19 +0700

    Initial Commit

---
+++ Stretchyheader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stretchyheader
+//
+//  Created by ahmad shiddiq on 21/02/19.
+//  Copyright © 2019 ahmad shiddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
